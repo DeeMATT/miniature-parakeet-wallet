cdae7d61eb642f1b83b3952089d8445c79ecc044
DeeMATT <mattode@outlook.com>
2021-06-16 10:52:26 +0100

    Error views

--- wallet/errors/views.py
+++ wallet/errors/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from enum import IntEnum
+from .models import Error
+from django.db import IntegrityError
 
-# Create your views here.
+class ErrorCodes(IntEnum):
+    GENERIC_ERROR = 0
+    UNAUTHENTICATED_REQUEST = 1
+    UNAUTHORIZED_REQUEST = 2
+
+
+# base error
+def getError(code, defaultMessage):
+    try:
+        _, _ = Error.objects.get_or_create(
+            code=code, description=defaultMessage)
+
+        return {'errorCode': code, 'message': defaultMessage}
+
+    except IntegrityError:
+        return {'errorCode': code, 'message': defaultMessage}
+
