c4c71e38222cfc7cbdebfd7471f11442013630be
DeeMATT <mattode@outlook.com>
2021-06-17 05:39:31 +0100

    model updated

--- wallet/wallet_manager/models.py
+++ wallet/wallet_manager/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class UserWalletData(models.Model):
-    first_name = models.TextField()
-    last_name = models.TextField()
-    email_address = models.EmailField()
-    phone_number = models.TextField()
+    first_name = models.TextField(null=True)
+    last_name = models.TextField(null=True)
+    email_address = models.EmailField(null=True)
+    phone_number = models.TextField(null=True)
     bvn = models.TextField(null=True)
-    birthday = models.DateField()
-    created_at = models.DateTimeField()
+    birthday = models.TextField(null=True)
+    created_at = models.TextField(null=True)
 
-    password = models.TextField()
-    account_no = models.TextField()
-    bank_name = models.TextField()
-    account_name = models.TextField()
-    available_balance = models.TextField()
+    password = models.TextField(null=True)
+    account_no = models.TextField(null=True)
+    bank_name = models.TextField(null=True)
+    account_name = models.TextField(null=True)
+    available_balance = models.TextField(null=True)
