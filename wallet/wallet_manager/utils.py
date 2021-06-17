23b8014146e2fdebc126743f73a711770e485349
DeeMATT <mattode@outlook.com>
2021-06-17 05:40:14 +0100

    createUserWalletData util

---
+++ wallet/wallet_manager/utils.py
@@ -0,0 +1,29 @@
+from .models import UserWalletData
+import logging
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+
+def createUserWalletData(data):
+    try:
+        object = UserWalletData.objects.create(
+            first_name=data.get('FirstName'),
+            last_name=data.get('LastName'),
+            email_address=data.get('Email'),
+            phone_number=data.get('PhoneNumber'),
+            bvn=data.get('BVN'),
+            birthday=data.get('DateOfBirth'),
+            created_at=data.get('DateSignedup'),
+            password=data.get('Password'),
+            account_no=data.get('AccountNo'),
+            bank_name=data.get('Bank'),
+            account_name=data.get('AccountName'),
+            available_balance=data.get('AvailableBalance')
+        )
+        return object
+
+    except Exception as err:
+        logger.error("createUserWalletData@Error")
+        logger.error(err)
+        return None
