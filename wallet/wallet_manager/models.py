70f45c960be4e09bf88e00575ac7ed6fc5d6f8a3
DeeMATT <mattode@outlook.com>
2021-06-17 04:04:18 +0100

    UserWalletData

--- wallet/wallet_manager/models.py
+++ wallet/wallet_manager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class UserWalletData(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email_address = models.EmailField()
+    phone_number = models.TextField()
+    bvn = models.TextField(null=True)
+    birthday = models.DateField()
+    created_at = models.DateTimeField()
+
+    password = models.TextField()
+    account_no = models.TextField()
+    bank_name = models.TextField()
+    account_name = models.TextField()
+    available_balance = models.TextField()
